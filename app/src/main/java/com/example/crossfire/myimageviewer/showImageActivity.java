1239b6f514bbd7024b0f8ae9fa71ccbea9ac95b8
CBike1103 <402220265@qq.com>
2016-07-12 10:57:09 +0800

    Add a gridView that can see all the pictures in external storage. Used LruCache.

---
+++ app/src/main/java/com/example/crossfire/myimageviewer/showImageActivity.java
@@ -0,0 +1,9 @@
+package com.example.crossfire.myimageviewer;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by CrossFire on 2016/7/12.
+ */
+public class showImageActivity extends AppCompatActivity{
+}
